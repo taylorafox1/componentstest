57362572534e274930e6f979b2c3584c3d3cf6ae
taylorafox1 <taylorafox1@gmail.com>
2018-11-15 11:40:08 -0500

    added props to commentdetail

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail />
+      <CommentDetail author="sam"/>
     </div>
   )
 }
