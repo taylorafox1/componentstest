5bead56e4b96cbd49b793ce50f74a1dbca331e33
taylorafox1 <taylorafox1@gmail.com>
2018-11-15 10:31:07 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import faker from 'faker';
+
+const App = () => {
+  return (
+    <div className='ui container comments'>
+      
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
+
