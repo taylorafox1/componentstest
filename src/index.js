3d5b978995ed42ba6aeda61b2419b617880b32c7
taylorafox1 <taylorafox1@gmail.com>
2018-11-15 10:51:30 -0500

    imported commentdetail

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
-
+import CommentDetail from './CommentDetail';
 const App = () => {
   return (
     <div className='ui container comments'>
