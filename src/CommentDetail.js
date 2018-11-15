bc273e5d570b0dd2d2da6ccde246c65ee3a6cb10
taylorafox1 <taylorafox1@gmail.com>
2018-11-15 12:08:31 -0500

    passed props from index to commentdetail

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -1,20 +1,20 @@
 import React from 'react';
-import faker from 'faker'; 
+//import faker from 'faker'; 
 
-const CommentDetail = (props) => {
+const CommentDetail = props => {
   return (
     <div className='comment'>
         <a href='/' className='avatar'>
-          <img alt="avatar" src={faker.image.avatar()}/>
+          <img alt="avatar" src={props.avatar}/>
         </a>
         <div className='content'>
           <a href='/' className='author'>
             {props.author}
           </a>
           <div className='metadata'>
-            <span className='date'>Today at 6:00pm</span>
+            <span className='date'>{props.timeAgo}</span>
           </div>
-          <div className='text'>Nice post!</div>
+          <div className='text'>{props.commentText}</div>
         </div>
       </div>
   )
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import faker from 'faker';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
   return (
     <div className='ui container comments'>
-      <CommentDetail author="sam"/>
+      <CommentDetail 
+        author="sam" 
+        timeAgo="Today at 4:45PM" 
+        commentText="nice blog post!" 
+        avatar={faker.image.avatar()}
+      />
     </div>
   )
 }
